da9cbb6b7474bf338c91d03df9f36b1cbcac1c75
Sadlongvip <sadlongvip@gmail.com>
2026-09-16 14:33:04 +0700

    update: update project slot 3

--- Slot3/product-service/src/main/java/com/fudn/product_service/ProductServiceApplication.java
+++ Slot3/product-service/src/main/java/com/fudn/product_service/ProductServiceApplication.java
@@ -1,7 +1,14 @@
 package com.fudn.product_service;
 
+import java.math.BigDecimal;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.fudn.product_service.model.Product;
+import com.fudn.product_service.repository.IProductRepository;
 
 @SpringBootApplication
 public class ProductServiceApplication {
@@ -10,4 +17,29 @@ public class ProductServiceApplication {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner loadData(IProductRepository productRepository) {
+		return args -> {
+			if (productRepository.count() == 0) {
+				Product product1 = Product.builder()
+						.name("Iphone 15")
+						.description("Apple Smartphone")
+						.price(BigDecimal.valueOf(1000))
+						.build();
+
+				Product product2 = Product.builder()
+						.name("Samsung Galaxy S24")
+						.description("Samsung Smartphone")
+						.price(BigDecimal.valueOf(900))
+						.build();
+
+				productRepository.save(product1);
+				productRepository.save(product2);
+
+				System.out.println("Đã thêm dữ liệu mẫu vào MongoDB!");
+			} else {
+				System.out.println("Dữ liệu đã tồn tại trong MongoDB!");
+			}
+		};
+	}
 }
--- Slot3/product-service/src/main/java/com/fudn/product_service/service/ProductService.java
+++ Slot3/product-service/src/main/java/com/fudn/product_service/service/ProductService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import com.fudn.product_service.dto.ProductRequest;
 import com.fudn.product_service.dto.ProductResponse;
 import com.fudn.product_service.model.Product;
-import com.fudn.product_service.repository.ProductRepository;
+import com.fudn.product_service.repository.IProductRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,10 +17,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ProductService {
 
-    private final ProductRepository productRepository;
+    private final IProductRepository productRepository;
 
     public ProductResponse createProduct(ProductRequest productRequest) {
-        //Ánh xạ từ ProductRequest sang Product entity nếu cần thiết,
+        // Ánh xạ từ ProductRequest sang Product entity nếu cần thiết,
         // sau đó lưu vào cơ sở dữ liệu thông qua repository.
 
         Product product = Product.builder()
@@ -34,18 +34,19 @@ public class ProductService {
 
         log.info("Product {} saved..", product.getId());
 
-        //Ánh xạ từ Product entity sang ProductResponse để trả về cho client.
+        // Ánh xạ từ Product entity sang ProductResponse để trả về cho client.
         return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice());
         // Logic to create a product
     }
 
     public List<ProductResponse> getAllProducts() {
-        //Lấy tất cả sản phẩm từ cơ sở dữ liệu thông qua repository,
+        // Lấy tất cả sản phẩm từ cơ sở dữ liệu thông qua repository,
         // sau đó ánh xạ chúng sang ProductResponse để trả về cho client.
         List<Product> products = productRepository.findAll();
         return products.stream()
                 .map(product -> new ProductResponse(product.getId(),
                         product.getName(), product.getDescription(),
-                        product.getPrice())).toList();
+                        product.getPrice()))
+                .toList();
     }
 }
