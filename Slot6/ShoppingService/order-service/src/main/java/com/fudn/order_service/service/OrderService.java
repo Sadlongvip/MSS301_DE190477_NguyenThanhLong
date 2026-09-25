27874eb6f639174c5258deb55079ea54aae51cfe
Sadlongvip <sadlongvip@gmail.com>
2026-09-25 16:42:57 +0700

    update(OrderService): Test Case 2 – Đơn vượt tồn kho thỏa mãn testcase

--- Slot6/ShoppingService/order-service/src/main/java/com/fudn/order_service/service/OrderService.java
+++ Slot6/ShoppingService/order-service/src/main/java/com/fudn/order_service/service/OrderService.java
@@ -19,8 +19,17 @@ public class OrderService {
     private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        var order = mapToOrder(orderRequest);
-        orderRepository.save(order);
+        boolean inStock = inventoryClient.isInStock(
+                orderRequest.skuCode(),
+                orderRequest.quantity());
+
+        if (inStock) {
+            var order = mapToOrder(orderRequest);
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException(
+                    "Product with Skucode " + orderRequest.skuCode() + " is not in stock");
+        }
     }
 
     private static Order mapToOrder(OrderRequest orderRequest) {
